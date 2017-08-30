2162102b6a1cdc361fdc02958f437f7ff7a0906b
hddhyq <recoverhdd@163.com>
2017-08-30 17:24:22 +0800

    add scroll

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
 
@@ -11,6 +12,10 @@ Vue.config.productionTip = false
 /* eslint-disable no-unused-vars */
 
 fastclick.attach(document.body)
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
