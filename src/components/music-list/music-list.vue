d4177755fa0a48574aa14a1d98323a23a30b6b71
hddhyq <recoverhdd@163.com>
2017-09-11 18:59:24 +0800

    添加了player 可以控制播放

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -18,7 +18,7 @@
             :data="songs" class="list" ref="list"
             :probe-type="probeType" :listen-scroll="listenScroll">
       <div class="song-list-wrapper">
-        <song-list :songs="songs"></song-list>
+        <song-list @select="selectItem" :songs="songs"></song-list>
       </div>
       <div class="loading-container" v-show="!songs.length">
         <loading></loading>
@@ -32,6 +32,7 @@
   import SongList from 'base/song-list/song-list'
   import Loading from 'base/loading/loading'
   import {prefixStyle} from 'common/js/dom'
+  import {mapActions} from 'vuex'
 
   const RESERVED_HEIGHT = 40
   const transform = prefixStyle('transform')
@@ -63,7 +64,16 @@
       },
       back() {
         this.$router.back()
-      }
+      },
+      selectItem(item, index) {
+        this.selectPlay({
+          list: this.songs,
+          index
+        })
+      },
+      ...mapActions([
+        'selectPlay'
+      ])
     },
     computed: {
       bgStyle() {
