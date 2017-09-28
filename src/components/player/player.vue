da19753968e8d66ba85915f2f62cfa9cf882ed6d
hddhyq <recoverhdd@163.com>
2017-09-28 09:48:29 +0800

    添加了confirm组件

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -94,11 +94,12 @@
                class="icon-mini"></i>
           </progress-circle>
         </div>
-        <div class="control">
+        <div class="control" @click.stop="showPlaylist">
           <i class="icon-playlist"></i>
         </div>
       </div>
     </transition>
+    <playlist ref="playlist"></playlist>
     <audio ref="audio" :src="currentSong.url"
            @canplay="ready" @error="error"
            @timeupdate="updateTime"
@@ -116,6 +117,7 @@
   import {shuffle} from 'common/js/util'
   import Lyric from 'lyric-parser'
   import Scroll from 'base/scroll/scroll'
+  import Playlist from 'components/playlist/playlist'
 
   const transform = prefixStyle('transform')
   const transitionDuration = prefixStyle('transitionDuration')
@@ -392,6 +394,9 @@
         this.$refs.middleL.style[transitionDuration] = `${time}ms`
         this.touch.initiated = false
       },
+      showPlaylist() {
+        this.$refs.playlist.show()
+      },
       _pad(num, n = 2) {
         let len = num.toString().length
         while (len < n) {
@@ -449,7 +454,8 @@
     components: {
       ProgressBar,
       ProgressCircle,
-      Scroll
+      Scroll,
+      Playlist
     }
   }
 </script>
